a9e5194eb82f4504262e4af5cf126e0847d790c9
sx-tane <117157108+sx-tane@users.noreply.github.com>
2025-06-08 14:02:49 +0900

    chore(database): update database connection string and modify SQL setup script
    - Changed the database connection string in package.json to point to the new Supabase instance.
    - Updated common_setup.sql to set the timezone for the new database and removed unnecessary schema creation commands.

--- prisma/scripts/common_setup.sql
+++ prisma/scripts/common_setup.sql
@@ -1,9 +1,5 @@
-ALTER DATABASE tourii_backend SET timezone TO 'Asia/Tokyo';
+-- ALTER DATABASE tourii_backend SET timezone TO 'Asia/Tokyo' IF EXISTS;
+ALTER DATABASE postgres SET timezone TO 'Asia/Tokyo';
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Create schemas for development and staging
-CREATE SCHEMA IF NOT EXISTS touriidev;
-CREATE SCHEMA IF NOT EXISTS touriistg;
-
 
