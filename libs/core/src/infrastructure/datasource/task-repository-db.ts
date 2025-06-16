8fa5945aa24f974c44ac366657c3f9c88c4792cc
namnguyen <nam.nguyen@agoda.com>
2025-06-16 20:02:03 +0700

    Add failed case

--- libs/core/src/infrastructure/datasource/task-repository-db.ts
+++ libs/core/src/infrastructure/datasource/task-repository-db.ts
@@ -1,6 +1,5 @@
-import { TaskRepository } from '@app/core/domain/game/quest/task.repository';
+import { SubmitTaskResponse, TaskRepository } from '@app/core/domain/game/quest/task.repository';
 import { PrismaService } from '@app/core/provider/prisma.service';
-import { SubmitTaskResponseDto } from '@app/tourii-backend/controller/model/tourii-response/submit-tasks-response.model';
 import { Injectable, Logger } from '@nestjs/common';
 import { TaskStatus, TaskType, quest_task } from '@prisma/client';
 
@@ -14,7 +13,7 @@ export class TaskRepositoryDb implements TaskRepository {
         taskId: string,
         answer: string,
         userId: string,
-    ): Promise<SubmitTaskResponseDto> {
+    ): Promise<SubmitTaskResponse> {
         const task = await this.findTask(taskId);
         if (!task || task.task_type !== TaskType.ANSWER_TEXT) {
             return { success: false, message: 'Task not found' };
@@ -22,7 +21,7 @@ export class TaskRepositoryDb implements TaskRepository {
 
         const normalizedAnswer = answer.trim().toLowerCase();
         const correctAnswer = 'Answer'.trim().toLowerCase(); // Temp
-        this.logger.log(`Normalized answer: ${normalizedAnswer}`);
+        this.logger.log(`Normalized user answer: ${normalizedAnswer}`);
 
         if (normalizedAnswer === correctAnswer) {
             await this.prisma.user_task_log.create({
@@ -36,9 +35,20 @@ export class TaskRepositoryDb implements TaskRepository {
                     completed_at: new Date(),
                 },
             });
-            this.logger.log('User task log created');
             return { success: true, message: 'Correct answer' };
         }
+
+        await this.prisma.user_task_log.create({
+            data: {
+                user_id: userId,
+                quest_id: task.quest_id,
+                task_id: taskId,
+                status: TaskStatus.FAILED,
+                action: TaskType.ANSWER_TEXT,
+                user_response: answer,
+                completed_at: new Date(),
+            },
+        });
         return { success: false, message: 'Incorrect answer' };
     }
 
@@ -46,15 +56,16 @@ export class TaskRepositoryDb implements TaskRepository {
         taskId: string,
         selectedOptionIds: number[],
         userId: string,
-    ): Promise<SubmitTaskResponseDto> {
+    ): Promise<SubmitTaskResponse> {
         const task = await this.findTask(taskId);
         if (!task || task.task_type !== TaskType.SELECT_OPTION) {
             return { success: false, message: 'Task not found' };
         }
 
         const correctOptionIds = [1, 2]; // Temp
-        const isCorrect =
-            JSON.stringify(correctOptionIds.sort()) === JSON.stringify(selectedOptionIds.sort());
+        const selectedOptionIdsSorted = JSON.stringify(selectedOptionIds.sort());
+        const correctOptionIdsSorted = JSON.stringify(correctOptionIds.sort());
+        const isCorrect = selectedOptionIdsSorted === correctOptionIdsSorted;
 
         if (isCorrect) {
             await this.prisma.user_task_log.create({
@@ -71,6 +82,17 @@ export class TaskRepositoryDb implements TaskRepository {
             this.logger.log('User task log created');
             return { success: true, message: 'Correct answer' };
         }
+        await this.prisma.user_task_log.create({
+            data: {
+                user_id: userId,
+                quest_id: task.quest_id,
+                task_id: taskId,
+                status: TaskStatus.FAILED,
+                action: TaskType.SELECT_OPTION,
+                user_response: selectedOptionIdsSorted,
+                completed_at: new Date(),
+            },
+        });
         return { success: false, message: 'Incorrect answer' };
     }
 
@@ -79,7 +101,7 @@ export class TaskRepositoryDb implements TaskRepository {
         longitude: number,
         latitude: number,
         userId: string,
-    ): Promise<SubmitTaskResponseDto> {
+    ): Promise<SubmitTaskResponse> {
         const task = await this.findTask(taskId);
         if (!task || task.task_type !== TaskType.CHECK_IN) {
             return { success: false, message: 'Task not found' };
@@ -101,6 +123,17 @@ export class TaskRepositoryDb implements TaskRepository {
             });
             return { success: true, message: 'Check-in successful' };
         }
+        await this.prisma.user_task_log.create({
+            data: {
+                user_id: userId,
+                quest_id: task.quest_id,
+                task_id: taskId,
+                status: TaskStatus.FAILED,
+                action: TaskType.CHECK_IN,
+                user_response: `${longitude},${latitude}`,
+                completed_at: new Date(),
+            },
+        });
         return { success: false, message: 'Check-in failed' };
     }
 
